d9c3c2b7f8d55d8877c10aca8935efd22902ad1a
janejulia0707 <95378232+janejulia0707@users.noreply.github.com>
2025-06-13 22:03:09 -0600

    fix: use environment variable for port and fix typo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const cors_proxy = require('cors-anywhere');
 
 const host = 'localhost';
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 cors_proxy.createServer({
   originWhitelist: [], // Allow all origins
